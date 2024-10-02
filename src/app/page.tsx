f93aabb7c9f92e73b82087859d7890871635b76d
johnnycheuk <johnnycheuk2649@gmail.com>
2024-10-02 10:40:00 +0800

    Update subscribe button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,35 @@ export default function Home() {
   const [hasTelegram, setHasTelegram] = useState(false);
   const [mainButtonClicked, setMainButtonClicked] = useState(false);
 
+  const getInvoiceLink = async () => {
+    const response = await fetch('http://localhost:3000/invoice', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'Subscribe AnnaSings',
+        description: 'Subscribe AnnaSings',
+        currency: 'USD',
+        prices: [{ "label": "Subscription Fee", amount: 100000 }]
+      })
+    });
+    if (response.ok) {
+      const data = await response.json();
+      if (data.success) {
+        return data.data.invoiceLink;
+      }
+      else {
+        console.log('Failed to retrieve invoice link.', data.message);
+        return '';
+      }
+    }
+    else {
+      console.log('Failed to retrieve invoice link.', await response.text());
+      return '';
+    }
+  }
+
   useEffect(() => {
     const telegram = (window as any).Telegram;
     const app = telegram.WebApp;
@@ -15,11 +44,14 @@ export default function Home() {
       // app.MainButton.setParams({ color: "#4CAF50" });
       app.MainButton.show();
       app.MainButton.enable();
-      app.MainButton.onClick(() => {
-        app.sendData("Button clicked");
+      app.MainButton.onClick(async () => {
+        const invoiceLink = await getInvoiceLink();
+        app.openInvoice(invoiceLink);
       })
-      document.querySelector('#btn')?.addEventListener('click', () => {
-        app.sendData("Button clicked");
+      document.querySelector('#btn')?.addEventListener('click', async () => {
+        // app.sendData("Button clicked");
+        const invoiceLink = await getInvoiceLink();
+        app.openInvoice(invoiceLink);
       })
     }
   }, []);
